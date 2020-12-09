e190f10de623cc67be906daab8a916f769951ca6
AndyThomas-Dev <andythomas.dev@gmail.com>
2020-12-09 19:14:48 +0000

    added 404 error for projects

--- personal_portfolio/projects/views.py
+++ personal_portfolio/projects/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from projects.models import Project
+from django.shortcuts import render
+from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 def project_index(request):
-    projects = Project.objects.all()
-    context = {"projects": projects}
+    try:
+        projects = Project.objects.all()
+        context = {"projects": projects}
+    except Project.DoesNotExist:
+        return HttpResponseNotFound("Page not found")
     return render(request, "project_index.html", context)
 
 
 def project_detail(request, pk):
-    project = Project.objects.get(pk=pk)
-    context = {"project": project}
+    try:
+        project = Project.objects.get(pk=pk)
+        context = {"project": project}
+    except Project.DoesNotExist:
+        return HttpResponseNotFound("Page not found")
     return render(request, "project_detail.html", context)
